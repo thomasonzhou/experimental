56ea844d780f0352e4b3515e1d198efafb44b040
Thomason Zhou <t95zhou@uwaterloo.ca>
2025-08-12 09:22:20 -0700

    Pre-commit hooks

--- core/mat.hpp
+++ core/mat.hpp
@@ -1,17 +1,18 @@
 #pragma once
 
-#include "stb/stb_image.h"
-#include "stb/stb_image_write.h"
-
 #include <memory>
 #include <string>
 
-namespace core{
-    class Mat{
-    public:
-        Mat(const std::string filename);
-    private:
-        std::unique_ptr<unsigned char, decltype(&stbi_image_free)> img_ptr;
-        int width, height, channels;
-    };
+#include "stb/stb_image.h"
+#include "stb/stb_image_write.h"
+
+namespace core {
+class Mat {
+ public:
+  Mat(const std::string filename);
+
+ private:
+  std::unique_ptr<unsigned char, decltype(&stbi_image_free)> img_ptr;
+  int width, height, channels;
 };
+};  // namespace core
--- tools/load.cpp
+++ tools/load.cpp
@@ -1,23 +1,23 @@
+#include <gflags/gflags.h>
+#include <stdio.h>
 
+#include <iostream>
+#include <memory>
+#include <string>
 
 #include "core/mat.hpp"
 
-#include <stdio.h>
-#include <memory>
-#include <string>
-#include <iostream>
-
-#include <gflags/gflags.h>
-
 DEFINE_string(image_path, "", "Path to the image file");
 
-int main(int argc, char* argv[]){
-    if(FLAGS_image_path.empty()){
-        std::cerr << "No image path provided. Use --image_path to specify the image file." << std::endl;
-        return 1;
-    }
+int main(int argc, char* argv[]) {
+  if (FLAGS_image_path.empty()) {
+    std::cerr
+        << "No image path provided. Use --image_path to specify the image file."
+        << std::endl;
+    return 1;
+  }
 
-    core::Mat mat(FLAGS_image_path);
+  core::Mat mat(FLAGS_image_path);
 
-    return 0;
+  return 0;
 }
